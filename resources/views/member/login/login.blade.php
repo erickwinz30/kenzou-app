68b81298ad252ad2f7ebf407eb6509b1f83712dc
Erick Winata <erickwinz27@gmail.com>
2024-09-10 10:34:07 +0700

    Pemberian layout untuk login dan register

--- resources/views/member/login/login.blade.php
+++ resources/views/member/login/login.blade.php
@@ -1,165 +1,102 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('member.login.layout.main')
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+@section('container')
+  <div class="container">
 
-  <title>Login - Kenzou Drive Thru Car Wash Admin</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-  <!-- Favicons -->
-  <link href="{{ asset('img/icons/icon-72.png') }}" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link
-    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-    rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/quill/quill.snow.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/simple-datatables/style.css') }}" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href=" {{ asset('css/style.css') }}" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
-
-<body>
-
-  <main>
-    <div class="container">
-
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
-              @if (session()->has('success'))
-                <div class="row justify-content-center">
-                  <div class="alert alert-success alert-dismissible fade show col-lg-12 justify-content-center"
-                    role="alert">
-                    {{ session('success') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                  </div>
-                </div>
-              @endif
-
-              @if (session()->has('error'))
-                <div class="row justify-content-center">
-                  <div class="alert alert-danger alert-dismissible fade show col-lg-12 justify-content-center"
-                    role="alert">
-                    {{ session('error') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                  </div>
-                </div>
-              @endif
-
-              <div class="d-flex justify-content-center py-4">
-                <a href="index.html" class="logo d-flex align-items-center w-auto">
-                  <img src="{{ asset('img/icons/icon-152.png') }}" alt="" style="max-height: 6em">
-                  <span class="d-none d-lg-block text-center">Kenzou Drive Thru Car Wash</span>
-                </a>
-              </div><!-- End Logo -->
-
-              @if (session()->has('loginError'))
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                  {{ session('loginError') }}
+            @if (session()->has('success'))
+              <div class="row justify-content-center">
+                <div class="alert alert-success alert-dismissible fade show col-lg-12 justify-content-center"
+                  role="alert">
+                  {{ session('success') }}
                   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-              @endif
+              </div>
+            @endif
 
-              <div class="card mb-3">
-
-                <div class="card-body">
-
-                  <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Login ke Akun Member</h5>
-                    <p class="text-center small">Masukkan email & password untuk login</p>
-                  </div>
-
-                  <form action='/login' method="POST" class="row g-3 needs-validation" novalidate>
-                    @csrf
-                    <div class="col-12">
-                      <label for="email" class="form-label">Email</label>
-                      <div class="input-group has-validation">
-                        <input type="email" name="email" class="form-control" id="email"
-                          placeholder="Masukkan email..." @error('email') is-invalid @enderror required>
-                        {{-- <div class="invalid-feedback">Please enter your email.</div> --}}
-                        @error('email')
-                          <div class="invalid-feedback">
-                            {{ $message }}
-                          </div>
-                        @enderror
-                      </div>
-                    </div>
-
-                    <div class="col-12">
-                      <label for="password" class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" id="password"
-                        placeholder="Masukkan password..." required>
-                      <div class="invalid-feedback">Please enter your password!</div>
-                    </div>
-                    <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Login</button>
-                    </div>
-
-                    <div class="col-12">
-                      <p class="small mb-0">Belum memiliki akun? <a href="/register">Buat akun</a></p>
-                    </div>
-                  </form>
-
-                  <p class="small text-center mt-3">Atau</p>
-
-                  <div>
-                    <a href="/auth/google" class="btn btn-light w-100" style="border: 1px solid black"><i
-                        class="bi bi-google me-3"></i>Sign-in dengan
-                      Google</a>
-                  </div>
-
+            @if (session()->has('error'))
+              <div class="row justify-content-center">
+                <div class="alert alert-danger alert-dismissible fade show col-lg-12 justify-content-center"
+                  role="alert">
+                  {{ session('error') }}
+                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
+              </div>
+            @endif
+
+            <div class="d-flex justify-content-center py-4">
+              <a href="index.html" class="logo d-flex align-items-center w-auto">
+                <img src="{{ asset('img/icons/icon-152.png') }}" alt="" style="max-height: 6em">
+                <span class="d-none d-lg-block text-center">Kenzou Drive Thru Car Wash</span>
+              </a>
+            </div><!-- End Logo -->
+
+            @if (session()->has('loginError'))
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('loginError') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+            @endif
+
+            <div class="card mb-3">
+
+              <div class="card-body">
+
+                <div class="pt-4 pb-2">
+                  <h5 class="card-title text-center pb-0 fs-4">Login ke Akun Member</h5>
+                  <p class="text-center small">Masukkan email & password untuk login</p>
+                </div>
+
+                <form action='/login' method="POST" class="row g-3 needs-validation" novalidate>
+                  @csrf
+                  <div class="col-12">
+                    <label for="email" class="form-label">Email</label>
+                    <div class="input-group has-validation">
+                      <input type="email" name="email" class="form-control" id="email"
+                        placeholder="Masukkan email..." @error('email') is-invalid @enderror required>
+                      {{-- <div class="invalid-feedback">Please enter your email.</div> --}}
+                      @error('email')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="col-12">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control" id="password"
+                      placeholder="Masukkan password..." required>
+                    <div class="invalid-feedback">Please enter your password!</div>
+                  </div>
+                  <div class="col-12">
+                    <button class="btn btn-primary w-100" type="submit">Login</button>
+                  </div>
+
+                  <div class="col-12">
+                    <p class="small mb-0">Belum memiliki akun? <a href="/register">Buat akun</a></p>
+                  </div>
+                </form>
+
+                <p class="small text-center mt-3">Atau</p>
+
+                <div>
+                  <a href="/auth/google" class="btn btn-light w-100" style="border: 1px solid black"><i
+                      class="bi bi-google me-3"></i>Sign-in dengan
+                    Google</a>
+                </div>
+
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-      </section>
+    </section>
 
-    </div>
-  </main><!-- End #main -->
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-      class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('vendor/apexcharts/apexcharts.min.js') }}"></script>
-  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('vendor/chart.js/chart.umd.js') }}"></script>
-  <script src="{{ asset('vendor/echarts/echarts.min.js') }}"></script>
-  <script src="{{ asset('vendor/quill/quill.js') }}"></script>
-  <script src="{{ asset('vendor/simple-datatables/simple-datatables.js') }}"></script>
-  <script src="{{ asset('vendor/tinymce/tinymce.min.js') }}"></script>
-  <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
-
-
-  <!-- Template Main JS File -->
-  <script src="{{ asset('js/main.js') }}"></script>
-
-</body>
-
-</html>
+  </div>
+@endsection
--- resources/views/member/login/register.blade.php
+++ resources/views/member/login/register.blade.php
@@ -1,220 +1,138 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('member.login.layout.main')
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+@section('container')
+  <div class="container">
 
-  <title>Registrasi - Kenzou Drive Thru Car Wash Admin</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link
-    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-    rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/quill/quill.snow.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/simple-datatables/style.css') }}" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href=" {{ asset('css/style.css') }}" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-
-  <style>
-    #togglePassword:hover {
-      background-color: #0056b3;
-      border-color: #0056b3;
-    }
-  </style>
-</head>
-
-<body>
-
-  <main>
-    <div class="container">
-
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
-              @if (session()->has('error'))
-                <div class="row justify-content-center">
-                  <div class="alert alert-danger alert-dismissible fade show col-lg-12 justify-content-center"
-                    role="alert">
-                    {{ session('error') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                  </div>
-                </div>
-              @endif
-
-              <div class="d-flex justify-content-center py-4">
-                <a href="index.html" class="logo d-flex align-items-center w-auto">
-                  <img src="assets/img/logo.png" alt="">
-                  <span class="d-none d-lg-block text-center">Kenzou Drive Thru Car Wash</span>
-                </a>
-              </div><!-- End Logo -->
-
-              @if (session()->has('loginError'))
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                  {{ session('loginError') }}
+            @if (session()->has('error'))
+              <div class="row justify-content-center">
+                <div class="alert alert-danger alert-dismissible fade show col-lg-12 justify-content-center" role="alert">
+                  {{ session('error') }}
                   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
-              @endif
+              </div>
+            @endif
 
-              <div class="card mb-3">
+            <div class="d-flex justify-content-center py-4">
+              <a href="index.html" class="logo d-flex align-items-center w-auto">
+                <img src="{{ asset('img/icons/icon-152.png') }}" alt="" style="max-height: 6em">
+                <span class="d-none d-lg-block text-center">Kenzou Drive Thru Car Wash</span>
+              </a>
+            </div><!-- End Logo -->
 
-                <div class="card-body">
+            @if (session()->has('loginError'))
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('loginError') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+            @endif
 
-                  <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Buat Akun Member</h5>
-                    <p class="text-center small">Masukkan data diri anda untuk membuat akun</p>
-                  </div>
+            <div class="card mb-3">
 
-                  <form action="/register" method="POST" class="row g-3 needs-validation" novalidate>
-                    @csrf
-                    <div class="col-12">
-                      <label for="nama" class="form-label">Nama</label>
-                      <div class="input-group has-validation">
-                        <input type="text" name="nama" class="form-control" id="nama"
-                          placeholder="Masukkan nama..." @error('nama') is-invalid @enderror required>
-                        @error('nama')
-                          <div class="invalid-feedback">
-                            {{ $message }}
-                          </div>
-                        @enderror
-                      </div>
-                    </div>
+              <div class="card-body">
 
-                    <div class="col-12">
-                      <label for="email" class="form-label">Email</label>
-                      <div class="input-group has-validation">
-                        <input type="email" name="email" class="form-control" id="email"
-                          placeholder="Masukkan email..." @error('email') is-invalid @enderror required>
-                        @error('email')
-                          <div class="invalid-feedback">
-                            {{ $message }}
-                          </div>
-                        @enderror
-                      </div>
-                    </div>
+                <div class="pt-4 pb-2">
+                  <h5 class="card-title text-center pb-0 fs-4">Buat Akun Member</h5>
+                  <p class="text-center small">Masukkan data diri anda untuk membuat akun</p>
+                </div>
 
-                    <div class="col-12">
-                      <label for="nomor_telepon" class="form-label">No. Telepon</label>
-                      <div class="input-group has-validation">
-                        <input type="text" inputmode="numeric" name="nomor_telepon" class="form-control"
-                          id="nomor_telepon" placeholder="Masukkan no. telepon..."
-                          @error('nomor_telepon') is-invalid @enderror required>
-                        @error('nomor_telepon')
-                          <div class="invalid-feedback">
-                            {{ $message }}
-                          </div>
-                        @enderror
-                      </div>
-                    </div>
-
-                    <div class="col-12">
-                      <label for="tanggal_lahir" class="form-label">Tgl Lahir</label>
-                      <div class="input-group has-validation">
-                        <input type="date" name="tanggal_lahir" class="form-control" id="tanggal_lahir"
-                          placeholder="Masukkan tanggal_lahir..." @error('tanggal_lahir') is-invalid @enderror
-                          required>
-                        @error('tanggal_lahir')
-                          <div class="invalid-feedback">
-                            {{ $message }}
-                          </div>
-                        @enderror
-                      </div>
-                    </div>
-
-                    <div class="col-12">
-                      <label for="password" class="form-label">Password</label>
-                      <div class="input-group has-validation d-flex">
-                        <input type="password" name="password" class="form-control" id="password" required
-                          placeholder="Masukkan password..." @error('password') is-invalid @enderror>
-                        <button type="button" class="btn btn-outline-secondary" id="togglePassword"
-                          style="margin-left: 6px; border-radius: 0.375rem; padding: 0.375rem 0.75rem;">
-                          <i class="bi bi-eye"></i>
-                        </button>
-                      </div>
-                      @error('password')
+                <form action="/register" method="POST" class="row g-3 needs-validation" novalidate>
+                  @csrf
+                  <div class="col-12">
+                    <label for="nama" class="form-label">Nama</label>
+                    <div class="input-group has-validation">
+                      <input type="text" name="nama" class="form-control" id="nama"
+                        placeholder="Masukkan nama..." @error('nama') is-invalid @enderror required>
+                      @error('nama')
                         <div class="invalid-feedback">
                           {{ $message }}
                         </div>
                       @enderror
                     </div>
-                    <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Buat Account</button>
-                    </div>
-                    <div class="col-12">
-                      <p class="small mb-0">Sudah memiliki akun? <a href="{{ route('login') }}">Log in</a></p>
-                    </div>
-                  </form>
+                  </div>
 
+                  <div class="col-12">
+                    <label for="email" class="form-label">Email</label>
+                    <div class="input-group has-validation">
+                      <input type="email" name="email" class="form-control" id="email"
+                        placeholder="Masukkan email..." @error('email') is-invalid @enderror required>
+                      @error('email')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="col-12">
+                    <label for="nomor_telepon" class="form-label">No. Telepon</label>
+                    <div class="input-group has-validation">
+                      <input type="text" inputmode="numeric" name="nomor_telepon" class="form-control"
+                        id="nomor_telepon" placeholder="Masukkan no. telepon..."
+                        @error('nomor_telepon') is-invalid @enderror required>
+                      @error('nomor_telepon')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="col-12">
+                    <label for="tanggal_lahir" class="form-label">Tgl Lahir</label>
+                    <div class="input-group has-validation">
+                      <input type="date" name="tanggal_lahir" class="form-control" id="tanggal_lahir"
+                        placeholder="Masukkan tanggal_lahir..." @error('tanggal_lahir') is-invalid @enderror required>
+                      @error('tanggal_lahir')
+                        <div class="invalid-feedback">
+                          {{ $message }}
+                        </div>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="col-12">
+                    <label for="password" class="form-label">Password</label>
+                    <div class="input-group has-validation d-flex">
+                      <input type="password" name="password" class="form-control" id="password" required
+                        placeholder="Masukkan password..." @error('password') is-invalid @enderror>
+                      <button type="button" class="btn btn-outline-secondary" id="togglePassword"
+                        style="margin-left: 6px; border-radius: 0.375rem; padding: 0.375rem 0.75rem;">
+                        <i class="bi bi-eye"></i>
+                      </button>
+                    </div>
+                    @error('password')
+                      <div class="invalid-feedback">
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+                  <div class="col-12">
+                    <button class="btn btn-primary w-100" type="submit">Buat Account</button>
+                  </div>
+                  <div class="col-12">
+                    <p class="small mb-0">Sudah memiliki akun? <a href="{{ route('login') }}">Log in</a></p>
+                  </div>
+                </form>
+
+                <p class="small text-center mt-3">Atau</p>
+
+                <div>
+                  <a href="/auth/google" class="btn btn-light w-100" style="border: 1px solid black"><i
+                      class="bi bi-google me-3"></i>Sign-in dengan
+                    Google</a>
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-      </section>
+    </section>
 
-    </div>
-  </main><!-- End #main -->
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-      class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('vendor/apexcharts/apexcharts.min.js') }}"></script>
-  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('vendor/chart.js/chart.umd.js') }}"></script>
-  <script src="{{ asset('vendor/echarts/echarts.min.js') }}"></script>
-  <script src="{{ asset('vendor/quill/quill.js') }}"></script>
-  <script src="{{ asset('vendor/simple-datatables/simple-datatables.js') }}"></script>
-  <script src="{{ asset('vendor/tinymce/tinymce.min.js') }}"></script>
-  <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
-
-  <script>
-    //tombol tampilkan password
-    document.getElementById('togglePassword').addEventListener('click', function() {
-      const passwordInput = document.getElementById('password');
-      const icon = this.querySelector('i');
-      if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        icon.classList.remove('bi-eye');
-        icon.classList.add('bi-eye-slash');
-      } else {
-        passwordInput.type = 'password';
-        icon.classList.remove('bi-eye-slash');
-        icon.classList.add('bi-eye');
-      }
-    });
-  </script>
-
-
-  <!-- Template Main JS File -->
-  <script src="{{ asset('js/main.js') }}"></script>
-
-</body>
-
-</html>
+  </div>
+@endsection
