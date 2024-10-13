00dbdfd88c54e832ec00f0c54a43c8815f9cf235
Erick Winata <erickwinz27@gmail.com>
2024-10-14 03:20:53 +0700

    Memperbaiki sidebar yang kurang responsive pada tampilan layar sedang seperti layar ipad

--- resources/views/member/layout/sidebar.blade.php
+++ resources/views/member/layout/sidebar.blade.php
@@ -8,24 +8,21 @@
         <span class="d-none d-lg-block ms-2">Home</span>
       </a>
     </li><!-- End Dashboard Nav -->
+
     <li class="nav-item">
       <a class="nav-link collapsed" href="index.html">
         <i class="bi bi-gift me-0"></i>
         <span class="d-none d-lg-block ms-2">Voucher</span>
       </a>
     </li><!-- End Dashboard Nav -->
-    {{-- <li class="nav-item">
-      <a class="nav-link collapsed" href="index.html">
-        <i class="bi bi-house-door-fill me-2"></i>
-        <span class="d-none d-lg-block ms-2">Home</span>
-      </a>
-    </li><!-- End Dashboard Nav --> --}}
+
     <li class="nav-item">
       <a class="nav-link collapsed" href="index.html">
         <i class="bi bi-bar-chart me-0"></i>
         <span class="d-none d-lg-block ms-2">Leaderboard</span>
       </a>
     </li><!-- End Dashboard Nav -->
+
     <li class="nav-item">
       <a class="nav-link {{ Request::is('account') || Request::is('account/*') ? '' : 'collapsed' }}"
         href="{{ route('account') }}">
@@ -33,6 +30,6 @@
         <span class="d-none d-lg-block ms-2">More</span>
       </a>
     </li><!-- End Dashboard Nav -->
-  </ul>
 
+  </ul>
 </aside><!-- End Sidebar-->
